d4d49ce0d0e02c90522ba058d2ade148ef0e6b9e
Joe Riley <142203239+The-Jokar@users.noreply.github.com>
2023-10-05 13:21:31 +1100

    Task 5 solution (#4)
    my solution to task 5 using binary search
    
    Co-authored-by: JoeRiley05 <142203239+JoeRiley05@users.noreply.github.com>

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,34 @@
 from __future__ import annotations
 
 from mountain import Mountain
+from algorithms.binary_search import binary_search
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.mountains = []
+        self.mountain_difficulty = []
+        self.is_insert = False
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
+        if self.is_insert or mountain in self.mountains:
+            difficulty = mountain.difficulty_level
+            pos = binary_search(self.mountain_difficulty, difficulty)
+            if mountain not in self.mountains and difficulty in self.mountain_difficulty:
+                pos += 1
+            if mountain in self.mountains and difficulty in self.mountain_difficulty:
+                pos = self.mountains.index(mountain)
+            return pos
+        else:
+            raise KeyError
 
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+        self.is_insert = True
+        for mountain in mountains:
+            pos = self.cur_position(mountain)
+            self.mountains.insert(pos, mountain)
+            self.mountain_difficulty.insert(pos, mountain.difficulty_level)
+        self.is_insert = False
+
+    def __str__(self):
+        return self.mountains
