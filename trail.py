814229049a385be1e4fab8f33cd52a662be0b655
Joe Riley <142203239+The-Jokar@users.noreply.github.com>
2023-10-05 13:25:22 +1100

    Task2 joe (#3)
    * Added Joe as a group member
    
    im gonna contribute a lot
    
    * My solution of task 1
    
    implemented all functions  from task 1 in
    
    * Task 2 solution
    
    my implementation of task 2
    
    ---------
    
    Co-authored-by: JoeRiley05 <142203239+JoeRiley05@users.noreply.github.com>

--- trail.py
+++ trail.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from mountain import Mountain
+from data_structures.linked_stack import LinkedStack
 
 from typing import TYPE_CHECKING, Union
 
@@ -25,7 +26,7 @@ class TrailSplit:
 
     def remove_branch(self) -> TrailStore:
         """Removes the branch, should just leave the remaining following trail."""
-        raise NotImplementedError()
+        return self.following.store
 
 @dataclass
 class TrailSeries:
@@ -44,34 +45,49 @@ class TrailSeries:
         Returns a *new* trail which would be the result of:
         Removing the mountain at the beginning of this series.
         """
-        raise NotImplementedError()
+        if self.mountain == None: 
+            raise ValueError
+        else:
+            return self.following.store
 
     def add_mountain_before(self, mountain: Mountain) -> TrailStore:
         """
         Returns a *new* trail which would be the result of:
         Adding a mountain in series before the current one.
         """
-        raise NotImplementedError()
+        try:
+            return TrailSeries(mountain, Trail(self))
+        except:
+            raise ValueError
 
     def add_empty_branch_before(self) -> TrailStore:
         """Returns a *new* trail which would be the result of:
         Adding an empty branch, where the current trailstore is now the following path.
         """
-        raise NotImplementedError()
+        try:
+            return TrailSplit(Trail(None), Trail(None), Trail(self))
+        except:
+            raise ValueError
 
     def add_mountain_after(self, mountain: Mountain) -> TrailStore:
         """
         Returns a *new* trail which would be the result of:
         Adding a mountain after the current mountain, but before the following trail.
         """
-        raise NotImplementedError()
+        try:
+            return TrailSeries(self.mountain, Trail(TrailSeries(mountain, self.following)))
+        except:
+            raise ValueError
 
     def add_empty_branch_after(self) -> TrailStore:
         """
         Returns a *new* trail which would be the result of:
         Adding an empty branch after the current mountain, but before the following trail.
         """
-        raise NotImplementedError()
+        try:
+            return TrailSeries(self.mountain, Trail(TrailSplit(Trail(None), Trail(None), self.following)))
+        except:
+            raise ValueError
 
 TrailStore = Union[TrailSplit, TrailSeries, None]
 
@@ -85,27 +101,58 @@ class Trail:
         Returns a *new* trail which would be the result of:
         Adding a mountain before everything currently in the trail.
         """
-        raise NotImplementedError()
+        return Trail(TrailSeries(mountain, self))
 
     def add_empty_branch_before(self) -> Trail:
         """
         Returns a *new* trail which would be the result of:
         Adding an empty branch before everything currently in the trail.
         """
-        raise NotImplementedError()
+        return Trail(TrailSplit(Trail(None), Trail(None), self))
 
     def follow_path(self, personality: WalkerPersonality) -> None:
         """Follow a path and add mountains according to a personality."""
-        raise NotImplementedError()
+        from personality import PersonalityDecision
+        remaining_paths = LinkedStack()
+        no_paths = False
+        current_path = self.store
+
+        while not no_paths:
+            if isinstance(current_path, TrailSeries):
+                personality.add_mountain(current_path.mountain)
+                current_path = current_path.following.store
+
+            elif isinstance(current_path, TrailSplit):
+                remaining_paths.push(current_path.following)
+                choice = personality.select_branch(current_path.top, current_path.bottom)
+
+                if choice == PersonalityDecision.BOTTOM:
+                    current_path = current_path.bottom.store
+                
+                elif choice == PersonalityDecision.TOP:
+                    current_path = current_path.top.store
+
+                elif choice == PersonalityDecision.STOP:
+                    return
+            
+            elif len(remaining_paths) != 0:
+                temp = remaining_paths.pop()
+                current_path = temp.store
+            
+            else:
+                no_paths = True
+        
+        return
+        
 
     def collect_all_mountains(self) -> list[Mountain]:
         """Returns a list of all mountains on the trail."""
-        raise NotImplementedError()
+        
 
     def difficulty_maximum_paths(self, max_difficulty: int) -> list[list[Mountain]]: # Input to this should not exceed k > 50, at most 5 branches.
         # 1008/2085 ONLY!
-        raise NotImplementedError()
+        pass
 
     def difficulty_difference_paths(self, max_difference: int) -> list[list[Mountain]]: # Input to this should not exceed k > 50, at most 5 branches.
         # 1054 ONLY!
-        raise NotImplementedError()
+        pass
